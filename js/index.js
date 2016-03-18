1b489b0a35216a49c9b37c700544b2da5e567e0e
ctaylor12 <digi_freek@Chelseas-MacBook-Pro.local>
2016-03-18 11:26:23 -0700

    v0

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Note: Plugin has been updated. Use version 3.1
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+// Refer: http://simpleweatherjs.com
+
+var newCity = prompt();
+
+$.simpleWeather({
+    location: newCity,
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+    
+      // Entire weather object
+      console.log();
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
+  
+  });
